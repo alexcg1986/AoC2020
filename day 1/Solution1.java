75051ac3850b79ef0e0114bad1aae6154bf415ee
Alex <alexcondegomez@gmail.com>
2021-04-01 18:25:31 +0200

    Day 1 Solutions

---
+++ day 1/Solution1.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.LinkedList;
+
+// --- Day 1: Report Repair ---
+// After saving Christmas five years in a row, you've decided to take a vacation
+// at a nice resort on a tropical island. Surely, Christmas will go on without
+// you.
+
+// The tropical island has its own currency and is entirely cash-only. The gold
+// coins used there have a little picture of a starfish; the locals just call
+// them stars. None of the currency exchanges seem to have heard of them, but
+// somehow, you'll need to find fifty of these coins by the time you arrive so
+// you can pay the deposit on your room.
+
+// To save your vacation, you need to get all fifty stars by December 25th.
+
+// Collect stars by solving puzzles. Two puzzles will be made available on each
+// day in the Advent calendar; the second puzzle is unlocked when you complete
+// the first. Each puzzle grants one star. Good luck!
+
+// Before you leave, the Elves in accounting just need you to fix your expense
+// report (your puzzle input); apparently, something isn't quite adding up.
+
+// Specifically, they need you to find the two entries that sum to 2020 and then
+// multiply those two numbers together.
+
+// For example, suppose your expense report contained the following:
+
+// 1721
+// 979
+// 366
+// 299
+// 675
+// 1456
+// In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying
+// them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+
+// Of course, your expense report is much larger. Find the two entries that sum
+// to 2020; what do you get if you multiply them together?
+
+class Solution1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> values = getValues();
+        while (!checkValues(values) && values.size() > 1)
+            values.pollFirst();
+    }
+
+    private static LinkedList<Integer> getValues() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            // populate linkedList
+            br.lines().forEach(line -> linkedList.push(Integer.valueOf(line)));
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return linkedList;
+    }
+
+    // iterate over thelinkedList and check conditions
+    private static boolean checkValues(LinkedList<Integer> linkedList) {
+        for (int i = 1; i < linkedList.size(); i++) {
+            if (linkedList.get(i) + linkedList.get(0) == 2020) {
+                System.out.println("The answer is :" + linkedList.get(i) * linkedList.get(0));
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ day 1/Solution2.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.LinkedList;
+
+// --- Part Two ---
+// The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation.
+// They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+// Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+// In your expense report, what is the product of the three entries that sum to 2020?
+
+class Solution2 {
+    public static void main(String[] args) {
+        LinkedList<Integer> values = getValues();
+        while (!checkValues(values) && values.size() > 1)
+            values.pollFirst();
+    }
+
+    private static LinkedList<Integer> getValues() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+            // populate linkedList
+            br.lines().forEach(line -> linkedList.push(Integer.valueOf(line)));
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return linkedList;
+    }
+
+    // iterate over thelinkedList and check conditions
+    private static boolean checkValues(LinkedList<Integer> linkedList) {
+        for (int i = 1; i < linkedList.size(); i++) {
+            int temporal;
+            if ((temporal = linkedList.get(i) + linkedList.get(0)) < 2020) {
+                for (int j = i + 1; j < linkedList.size(); j++) {
+                    if (temporal + linkedList.get(j) == 2020) {
+                        System.out
+                                .println("The answer is: " + linkedList.get(j) * linkedList.get(i) * linkedList.get(0));
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
