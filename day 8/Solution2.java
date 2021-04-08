239913aaeac955ac72700a3fdba81ce438db320e
alexcg1986 <alexcondegomez@gmail.com>
2021-04-08 12:42:53 +0200

    removed unneccesary imports

--- day 8/Solution2.java
+++ day 8/Solution2.java
@@ -2,9 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.BitSet;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 // --- Part Two ---
 // After some careful analysis, you believe that exactly one instruction is
